286bf8527f0a3d305472b1c38c3e4495cdb584b2
giovannimazzi <gioelor@libero.it>
2026-01-21 16:11:09 +0100

    snack 1 - risolto

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,17 @@ const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// # SOLUZIONE
+names.forEach((name) => console.log(name));
+
+// ## alternativa 1
+console.log(...names);
+
+// ## alternativa 2
+console.log(names.join(", "));
+
+// ## alternativa 3
+console.log(
+  names.reduce((output, name) => (output += `'${name}', `), "").slice(0, -2),
+);
