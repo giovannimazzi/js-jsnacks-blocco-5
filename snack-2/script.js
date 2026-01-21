62caf40cecc62a19ced576865c7544cb48260d4e
giovannimazzi <gioelor@libero.it>
2026-01-21 18:06:24 +0100

    snack 2 - risolto

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,18 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// # SOLUZIONE
+people.forEach(({ name }) => console.log(name));
+
+// ## alternativa 1
+console.log(
+  people
+    .reduce((output, { name }) => (output += `'${name}', `), "")
+    .slice(0, -2),
+);
